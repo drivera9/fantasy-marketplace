88bb0ff26bc650b20a1473f54423e78ff1d75970
DESKTOP-ICETJ62\David <davidrivera0218@gmail.com>
2022-02-07 18:10:00 -0500

    changes

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -9,11 +9,24 @@ import { BaseComponent } from '../base/base.component';
 })
 export class FooterComponent extends BaseComponent implements OnInit {
 
-  constructor(router: Router) { 
+  constructor(router: Router) {
     super(router);
   }
 
   ngOnInit() {
   }
 
+  goToPage(page: any) {
+    switch (page) {
+      case 'facebook':
+        window.open('https://www.facebook.com/fantasystudiocolombia', '_blank')
+        break;
+      case 'instagram':
+        window.open('https://www.instagram.com/fantasy_studio_colombia', '_blank')
+        break;
+      case 'twitter':
+        window.open('https://twitter.com/fantasystudiowc', '_blank')
+        break;
+    }
+  }
 }
